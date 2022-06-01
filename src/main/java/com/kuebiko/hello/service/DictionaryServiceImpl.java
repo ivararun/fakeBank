56976ad20b7139cae4d04daaecf04a9eed042097
manish khadka <kdkmanes>
2022-06-01 11:57:30 -0400

    updated

--- src/main/java/com/kuebiko/hello/service/DictionaryServiceImpl.java
+++ src/main/java/com/kuebiko/hello/service/DictionaryServiceImpl.java
@@ -1,20 +1,25 @@
 package com.kuebiko.hello.service;
 
+import com.kuebiko.hello.dao.DictionaryDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class DictionaryServiceImpl implements DictionaryService{
 
-    private Map<String, String> map = new HashMap<>();
+    @Autowired
+   DictionaryDao dictionaryDao;
 
     @Override
     public String search(String searchQuery) {
-        String value = map.get(searchQuery);
-        return value;
+        return dictionaryDao.get(searchQuery);
     }
 
     @Override
-    public String add(String key, String value) {
-        return map.put(key, value);
+    public String add(String key, String value){
+        return null;
     }
 }
