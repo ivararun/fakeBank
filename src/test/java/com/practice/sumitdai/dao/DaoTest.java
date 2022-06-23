ebe10583f87e5c4e68c217a96d39ad611cf8b3cf
Suresh <mail2sghimire@gmail.com>
2022-06-23 09:06:15 -0700

    Added test case

--- src/test/java/com/practice/sumitdai/dao/DaoTest.java
+++ src/test/java/com/practice/sumitdai/dao/DaoTest.java
@@ -1,27 +1,115 @@
 package com.practice.sumitdai.dao;
 
+import com.practice.sumitdai.entity.ProfileEntity;
 import com.practice.sumitdai.model.ProfileModel;
 import com.practice.sumitdai.repo.ProfileRepo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
+import java.util.Optional;
+
+//@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
 public class DaoTest {
 
-    @Test
-    public void signup_nullModel(){
-        ProfileModel null_profileModel = null;
+    @InjectMocks
+    ProfileDao profileDao;
 
-        ProfileModel res_model = new ProfileDao().signup(null_profileModel);
-        Assertions.assertEquals(res_model, null_profileModel);
+    @Mock
+    ProfileRepo profileRepo;
+
+    @Test
+    public void signup_null_failure() {
+
+        ProfileModel response = profileDao.signup(null);
+        Assertions.assertNull(response);
+
     }
 
     @Test
-    public void signup_model_errorNull(){
-        //checking of the model passing on signup method has error msg or not
+    public void signup_validation_fail() {
+
+        ProfileModel inputModel = new ProfileModel();
+        ProfileModel response = profileDao.signup(inputModel);
+        Assertions.assertEquals("First name cannot be empty", response.getMessage());
+
     }
+
+    private ProfileModel getModel() {
+        ProfileModel inputModel = new ProfileModel();
+        inputModel.setFirstName("kjk");
+        inputModel.setMiddleName("kjddd");
+        inputModel.setLastName("last");
+        inputModel.setEmail("email");
+        inputModel.setSsn("123456789");
+        inputModel.setAddress("jgj");
+        inputModel.setGender("M");
+        inputModel.setPhoneNumber(1234567890L);
+        return inputModel;
+    }
+
+    @Test
+    public void signup_success() {
+
+        // when repo calls save methods, return yourn own data
+        ProfileEntity simulatedResponseFromDb = new ProfileEntity();
+        simulatedResponseFromDb.setId(123l);
+
+        Mockito.when(profileRepo.save(Mockito.any()))
+                .thenReturn(simulatedResponseFromDb);
+
+        ProfileModel respones_model = profileDao.signup(getModel());
+        Assertions.assertNotNull(respones_model.getId());
+
+    }
+
+    @Test
+    public void findById_id_null() {
+
+        long passed_id = 123l;
+
+        Mockito.when(profileRepo.findById(passed_id))
+                .thenReturn(Optional.empty());
+        ProfileEntity entity = profileDao.findById(passed_id);
+        Assertions.assertNull(entity);
+
+
+    }
+
+    @Test
+    public void findById_id_sucess() {
+
+        long passed_id = 123l;
+
+        Mockito.when(profileRepo.findById(passed_id))
+                .thenReturn(Optional.of(new ProfileEntity()));
+
+        ProfileEntity entity = profileDao.findById(passed_id);
+        Assertions.assertNotNull(entity);
+
+
+    }
+
+    //save method
+    @Test
+    public void save_sucess(){
+        ProfileEntity en = new ProfileEntity();
+
+        Mockito.when(profileRepo.save(en))
+                .thenReturn(new ProfileEntity());
+        ProfileEntity savedEnt= profileDao.save(en);
+
+        Assertions.assertNotNull(savedEnt);
+    }
+
+
 }
